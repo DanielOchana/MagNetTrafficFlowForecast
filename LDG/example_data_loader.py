2bf2c76cb3bcfaa862c7738808131147b660e768
DanielOchana <danielochana@gmail.com>
2024-07-30 11:54:14 +0300

    chore: Update time ranges and load adjacency matrices in example_data_loader.py

--- LDG/example_data_loader.py
+++ LDG/example_data_loader.py
@@ -11,56 +11,65 @@ class ExampleDataset(EventsDataset):
 
         if split == 'train':
             time_start = 0
-            time_end = datetime(2013, 8, 31, tzinfo=self.TZ).toordinal()
+            time_end = datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=12).toordinal()
         elif split == 'test':
-            time_start = datetime(2013, 9, 1, tzinfo=self.TZ).toordinal()
-            time_end = datetime(2014, 1, 1, tzinfo=self.TZ).toordinal()
+            time_start = datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=15).toordinal()
+            time_end = datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=0).toordinal()
         else:
             raise ValueError('invalid split', split)
 
-        self.FIRST_DATE = datetime(2012, 12, 28, tzinfo=self.TZ)
+        self.FIRST_DATE = datetime(2016, 1, 4, tzinfo=self.TZ)
 
-        self.TEST_TIMESLOTS = [datetime(2013, 9, 1, tzinfo=self.TZ),
-                               datetime(2013, 9, 25, tzinfo=self.TZ),
-                               datetime(2013, 10, 20, tzinfo=self.TZ),
-                               datetime(2013, 11, 15, tzinfo=self.TZ),
-                               datetime(2013, 12, 10, tzinfo=self.TZ),
-                               datetime(2014, 1, 1, tzinfo=self.TZ)]
+        self.TEST_TIMESLOTS = [
+            datetime(2016, 1, 4, tzinfo=self.TZ).replace(hour=6),
+            datetime(2016, 1, 4, tzinfo=self.TZ).replace(hour=9),
+            datetime(2016, 1, 4, tzinfo=self.TZ).replace(hour=12),
+            datetime(2016, 1, 4, tzinfo=self.TZ).replace(hour=15),
+            datetime(2016, 1, 4, tzinfo=self.TZ).replace(hour=18),
+            datetime(2016, 1, 4, tzinfo=self.TZ).replace(hour=21),
+            datetime(2016, 1, 5, tzinfo=self.TZ).replace(hour=6),
+            datetime(2016, 1, 5, tzinfo=self.TZ).replace(hour=9),
+            datetime(2016, 1, 5, tzinfo=self.TZ).replace(hour=12),
+            datetime(2016, 1, 5, tzinfo=self.TZ).replace(hour=15),
+            datetime(2016, 1, 5, tzinfo=self.TZ).replace(hour=18),
+            datetime(2016, 1, 5, tzinfo=self.TZ).replace(hour=21),
+            datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=6),
+            datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=9),
+            datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=12),
+            datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=15),
+            datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=18),
+            datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=21),
+            datetime(2016, 1, 6, tzinfo=self.TZ).replace(hour=0)
+        ]
 
 
 
-        self.N_nodes = 100
 
-        self.A_initial = np.random.randint(0, 2, size=(self.N_nodes, self.N_nodes))
-        self.A_last = np.random.randint(0, 2, size=(self.N_nodes, self.N_nodes))
+        self.A_initial = np.loadtxt('data_common_slots_csv/adj_matrices/Adj_0.csv', delimiter=',')
+        self.A_last = np.loadtxt('data_common_slots_csv/adj_matrices/Adj_17.csv', delimiter=',')
+        self.N_nodes = self.A_last.shape[1]
 
         print('\nA_initial', np.sum(self.A_initial))
         print('A_last', np.sum(self.A_last), '\n')
 
-        self.n_events = 10000
+        
         all_events = []
-        for i in range(self.n_events):
-            user_id1 = np.random.randint(0, self.N_nodes)
-            user_id2 = np.random.choice(np.delete(np.arange(self.N_nodes), user_id1))
-            ts = max((time_start, self.FIRST_DATE.toordinal()))
-            event_time = datetime.fromordinal(ts + np.random.randint(0, time_end - ts) )
-            assert event_time.timestamp() >= self.FIRST_DATE.timestamp(), (event_time, self.FIRST_DATE)
-            all_events.append((user_id1, user_id2, np.random.choice(['communication event',
-                                                                     'association event']), event_time))
+        edges_info = np.loadtxt('non_directed_graph_info/ID_edges_info.csv', delimiter=',')
+        all_events = edges_info.tolist()
 
         self.event_types = ['communication event']
 
         self.all_events = sorted(all_events, key=lambda t: t[3].timestamp())
         print('\n%s' % split.upper())
         print('%d events between %d users loaded' % (len(self.all_events), self.N_nodes))
-        print('%d communication events' % (len([t for t in self.all_events if t[2] == 1])))
-        print('%d assocition events' % (len([t for t in self.all_events if t[2] == 0])))
+        # print('%d communication events' % (len([t for t in self.all_events if t[2] == 1])))
+        # print('%d assocition events' % (len([t for t in self.all_events if t[2] == 0])))
 
         self.event_types_num = {'association event': 0}
         k = 1  # k >= 1 for communication events
         for t in self.event_types:
             self.event_types_num[t] = k
-            k += 1
+            # k += 1
 
         self.n_events = len(self.all_events)
 
